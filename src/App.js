74dd3331a567ebe48864362f9e014732a4cde1f8
Free Archer <bespalov.alexey@yandex.ru>
2019-04-08 16:52:16 +0700

    Заработал импорт закладок из файла

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
+import BkContainer from './components/BkContainer';
 
 class App extends Component {
-  state = {
-    bookmarks: null
-  }
+  constructor() {
+    super()
+    this.state = {
+      bookmarks: []
+    }
 
-  getBookmarksFromFile = () => {
-    const bk = import('./bookmarks.js')
-    bk.then((res) => {
-      const bookmarks = res.bk.children
-      this.setState({ bookmarks: bookmarks })
-    })
   }
 
   componentDidMount = () => {
-    this.getBookmarksFromFile()
+    this.setState({ bookmarks: this.props.bk })
+    // const myHeaders = new Headers({
+    //   "Content-Type": "application/json",
+    //   Accept: "application/json"
+    // })
+
+    // fetch("http://localhost:3000/bookmarks.json", { headers: myHeaders })
+    //   .then(response => {
+    //     console.log(response);
+    //     this.setState({ response });
+    //     return response.json();
+    //   })
+    //   .then(data => {
+    //     console.log(this);
+    //     this.setState({ bookmarks: data.children });
+    //     console.log(this.bookmarks);
+    //   })
+    //   .catch(err => {
+    //     console.log(err)
+    //   })
+
   }
 
   render() {
+    const bookmarks = this.state.bookmarks.children
+    console.log(bookmarks)
+
+    if (!bookmarks) return ('Is Empty');
+
     return (
       <div>
-        {console.log(this.state.bookmarks)}
-
+        {bookmarks.map((currentBk) => (
+          <BkContainer
+            key={currentBk.id}
+            bkFolder={currentBk.children}
+            title={currentBk.title}
+          />
+        )
+        )
+        }
       </div>
     );
   }
---
+++ src/components/BkContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+export default class BkContainer extends Component {
+
+    render() {
+        return (
+            <p>!!!!!</p>
+        )
+    }
+}
